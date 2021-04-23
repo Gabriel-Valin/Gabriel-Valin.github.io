996070b227874bbf285bf80d8d07751374fa7328
Gabriel Valin <gabriel@4.events>
2021-04-23 00:40:38 -0300

    refactor front and add swal's

---
+++ assets/js/alerts.js
@@ -0,0 +1,10 @@
+function input() {
+    Swal.fire({
+        icon: 'info',
+        title: 'Contatos!',
+        html:
+            '<p>email: gabrielvalincontato@gmail.com</p>' +
+            '<a href="https://www.linkedin.com/in/gabriel-valin-1759801aa/">LinkedIn</a><br><br>' +
+            '<a href="https://api.whatsapp.com/send?phone=5514996928608">WhatsApp</a>',
+    })
+}
